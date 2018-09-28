9c7e5972e6652e03cb94801ba0a73e1471edc942
AnjaDannbg <anja.dannbg@gmail.com>
2018-09-28 18:02:20 +0200

    thumbnail and carousel revision

--- js/jquery.elastislide.js
+++ js/jquery.elastislide.js
@@ -340,7 +340,7 @@
 			this.imgSize = { width : $img.outerWidth( true ), height : $img.outerHeight( true ) };
 
 			this._setItemsSize();
-			this.options.orientation === 'horizontal' ? this.$el.css( 'max-height', this.imgSize.height ) : this.$el.css( 'height', this.options.minItems * this.imgSize.height );
+			this.options.orientation === 'horizontal' ? this.$el.css( 'max-height', '120px' ) : this.$el.css( 'height', this.options.minItems * this.imgSize.height );
 
 			// add the controls
 			this._addControls();
